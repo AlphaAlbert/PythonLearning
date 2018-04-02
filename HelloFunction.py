eada64d08a78d8b730689cfed7fb5363b0bf3f03
AlphaAlbert <alpha.alberto.pacheco@gmail.com>
2018-04-02 16:41:57 -0400

    First Look at functions (hello world)

---
+++ HelloFunction.py
@@ -0,0 +1,5 @@
+def HelloWorld(): #Define a function and use () to pass parameters
+    print("Hello World") #body of the function
+
+HelloWorld() # CALLS the hello world function 
+HelloWorld() # functions can be called more than once 
